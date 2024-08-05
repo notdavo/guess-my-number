824c96a5caf12c2acce41a02235356364b0a02b0
notdavo <da.arce.97@gmail.com>
2024-08-05 12:24:39 -0600

    Adding features to the game

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+
+let score = 20;
+const messageText = document.querySelector('.message');
+const scoreText = document.querySelector('.score');
+const highscoreText = document.querySelector('.highscore');
+const guessText = document.querySelector('.guess');
+const secretNumberText = document.querySelector('.number');
+const button = document.querySelector('.check');
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(guessText.value);
+  if (!guess) {
+    changeText('No number!', messageText);
+  } else if (guess === secretNumber) {
+    changeText('Correct Number!', messageText);
+    checkHighScore();
+    changeText(String(secretNumber), secretNumberText);
+    button.disabled = true;
+  } else if (guess > secretNumber) {
+    changeText('Too high!', messageText);
+    if (score === 1) changeText('You loss!', messageText);
+    reduceScore();
+  } else if (guess < secretNumber) {
+    changeText('Too low!', messageText);
+    if (score === 1) changeText('You loss!', messageText);
+    reduceScore();
+  }
+});
+
+function reduceScore() {
+  scoreText.textContent = String((score -= 1));
+}
+
+function changeText(text, textField) {
+  textField.textContent = text;
+}
+
+function checkHighScore() {
+  if (score > Number(highscoreText.textContent))
+    changeText(String(score), highscoreText);
+}
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  changeText(String(score), scoreText);
+  button.disabled = false;
+  guessText.value = '';
+  changeText('?', secretNumberText);
+  changeText('Start guessing...', messageText);
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+});
